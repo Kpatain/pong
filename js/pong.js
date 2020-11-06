bb9a458fc43fbf8d13c1cf796dbf507fc62c3e0f
Kpatain <62848924+Kpatain@users.noreply.github.com>
2020-11-06 22:22:53 +0100

    commentary

--- js/pong.js
+++ js/pong.js
@@ -1,13 +1,16 @@
 
-
 let largeur=$("#balle").width();
 let gauche =parseInt($("#balle").css("left"));
 let haut =parseInt($("#balle").css("top"));
+
+//alerte taille
 alert(largeur);
 
 setInterval(function(){
+    //Mouvement de la balle
     gauche = gauche+1;
     haut=haut+0.5;
+    
     $("#balle").css("left",gauche);
     $("#balle").css("top",haut);
 }, 10);
